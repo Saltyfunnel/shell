0754fdb43a68441aebba5838e0e5f656c08f99a4
Saltyfunnel <gerardsheal@gmail.com>
2026-06-01 21:43:20 +0100

    Update hyprland.lua

--- configs/hypr/hyprland.lua
+++ configs/hypr/hyprland.lua
@@ -35,7 +35,6 @@ hl.monitor({
 hl.on("hyprland.start", function()
   hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
   hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
-  hl.exec_cmd("swww-daemon --format xrgb")
   hl.exec_cmd("qs -c noctalia-shell")
   hl.exec_cmd("udiskie")
 end)
